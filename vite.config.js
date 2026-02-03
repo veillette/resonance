3c5270a5dd4082160efb793b9ae02ef7a14d1c46
Martin Veillette <martin.veillette@gmail.com>
2026-02-02 23:02:23 -0500

    fix config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
+import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // So the build can be served from an arbitrary path
   base: "./",
+  define: {
+    // Provide packageJSON for SceneryStack's update checker
+    "phet.chipper.packageJSON": JSON.stringify(packageJson),
+  },
   build: {
     rollupOptions: {
       output: {
